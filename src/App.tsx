80cf2e4823f39ee1fe80eeb8e50cb335d8c4d6c9
Daniel Merillas <dmerillasg@minsait.com>
2023-04-05 16:04:29 +0200

    añadida tabla pra realizar busqueda con todos los pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -2,83 +2,66 @@
 import './App.css';
 
 import { useEffect, useState } from 'react';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import { Card } from './components/Card';
+import { ReactTable } from './components/ReactTable';
 
 
 
 function App() {
 
-
   const [randomValue, setRandomValue] = useState<any>([]);
+  const [data, setData] = useState<any>([]);
   const numCards = 10
   const apiUrl = 'https://pokeapi.co/api/v2/pokemon'
 
-  const [query, setQuery] = useState('');
 
-  const handleInputChange = (event: any) => {
-    setQuery(event.target.value);
-  }
-
-  const handleSearchClick = () => {
-    setRandomValue([<Card pokemonUrl={`${apiUrl}/${query}`} />])
-  }
-
-  const handleKeyDown = (event:any) => {
-    if (event.key === "Enter") {
-      setRandomValue([<Card pokemonUrl={`${apiUrl}/${query}`} />])
-    }
-  };
 
   useEffect(() => {
-    const fetchData = async () => {
-
-      let randomValues: any = []
-      const response = await fetch(`${apiUrl}?offset=0&limit=2000`);
-      const jsonData = await response.json();
-
-
-      for (let i = 0; i < numCards; i++) {
-        const value = Math.floor(Math.random() * (jsonData.results.length)) + 1
-
-        randomValues.push(<Card pokemonUrl={jsonData.results[value].url} />)
-      }
-      setRandomValue(randomValues)
-    };
-
-    fetchData();
+    pintarPokemonsAleatoriamente();
   }, []);
 
-  
+
 
 
   return (
     <>
       <h1 className='title'>Pokedesk de Aarón</h1>
-      <div className="search-box">
-        <input
-          type="text"
-          placeholder="Buscar"
-          value={query}
-          onChange={handleInputChange}
-          onKeyDown={handleKeyDown}
-        />
-        <button onClick={handleSearchClick}>Buscar</button>
+      <div className='searchbox'>
+        <ReactTable data={data} filterFunction={setRandomValue} allFunction={pintarPokemonsAleatoriamente}/>
       </div>
-      
+      <Carousel>
+        {randomValue ?
+          randomValue
+          :
+          ''
+        }
 
-        <Carousel>
-          {randomValue ?
-            randomValue
-            :
-            ''
-          }
-
-        </Carousel>
+      </Carousel>
 
     </>
   );
+
+  function pintarPokemonsAleatoriamente() {
+    const fetchData = async () => {
+
+      let randomValues: any = [];
+      const response = await fetch(`${apiUrl}?offset=0&limit=2000`);
+      const jsonData = await response.json();
+
+
+      for (let i = 0; i < numCards; i++) {
+        const value = Math.floor(Math.random() * (jsonData.results.length)) + 1;
+
+        randomValues.push(<Card pokemonUrl={jsonData.results[value].url} />);
+      }
+      setRandomValue(randomValues);
+      setData(jsonData.results);
+    };
+
+    fetchData();
+  }
 }
 
 export default App;
---
+++ src/components/ReactTable.tsx
@@ -0,0 +1,80 @@
+import { TextField } from '@mui/material';
+import { useCallback, useEffect, useState } from 'react';
+import DataTable from 'react-data-table-component';
+import { Card } from './Card';
+
+
+
+
+
+export function ReactTable(props: { data: any; filterFunction: any; allFunction:any}) {
+    const [filterText1, setFilterText] = useState('');
+    const [selectedRows, setSelectedRows] = useState([])
+    const [filteredItems, setFilteredItems] = useState(props.data);
+
+    const handleRowSelected = useCallback((state: any) => {
+        setSelectedRows(state.selectedRows);
+    }, []);
+
+
+
+    const columns = [
+
+        {
+            name: 'Nombre',
+            selector: (row: any) => row.name,
+            sortable: true,
+        }
+    ];
+
+    useEffect(() => {
+
+        if (filterText1 && filterText1 !== "") {
+            const filter = props.data.filter(
+                (item: any) => {
+                    return (item.name && item.name.toLowerCase().includes(filterText1.toLowerCase()))
+
+                })
+
+            setFilteredItems(filter)
+        }
+        else {
+            setFilteredItems(props.data)
+        }
+
+
+    }, [filterText1, props.data])
+
+    useEffect(() => {
+
+        if(selectedRows && selectedRows.length > 0){
+            const filter = selectedRows.map((s: any) => <Card pokemonUrl={s.url} />)
+            props.filterFunction(filter)
+        }
+        else{
+            props.allFunction()
+        }
+        
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [selectedRows])
+
+
+    return (
+        <>
+            <TextField
+                className="search"
+                type="text"
+                placeholder="Buscar pokemon"
+                aria-label="Search Input"
+                value={filterText1}
+                onChange={(e: any) => setFilterText(e.target.value)}
+            />
+            <DataTable className='tablaReact' columns={columns} data={filteredItems} pagination selectableRows onSelectedRowsChange={handleRowSelected} selectableRowsHighlight={true}
+            paginationPerPage={5}
+            paginationRowsPerPageOptions={[5,10, 25, 50, 100]}
+            />
+        </>
+    )
+
+}
